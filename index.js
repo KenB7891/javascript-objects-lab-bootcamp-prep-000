7716af050fed1ad0d62f22dc1ae821020d570cf3
Kenneth Bilicke <kbilicke7891@gmail.com>
2018-03-07 03:15:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectsByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
